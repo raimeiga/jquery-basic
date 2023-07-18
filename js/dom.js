0a207da3674ea780090a7ff7de2a1c9e6ae99d1f
raimeiga <yusuke612@hotmail.com>
2023-07-18 17:56:00 +0900

    7.6 HTML要素を表示・非表示」のコード追加

--- js/dom.js
+++ js/dom.js
@@ -1,22 +1,11 @@
 $(function() {
-    //変数の初期化
-   let num = 0;
- 
-   // id属性がappendの要素がクリックされたら要素を追加
-   $('#append').on('click', function(){
-     // numを1ずつ増やす
-     num++;
-     $('ul').append('<li>appendで追加' + num + '</li>');
-   });
+     // id属性がshowの要素がクリックされたら要素を表示
+   $('#show').on('click', function(){
+    $('ul').show();
+  });
 
-   // id属性がremoveの要素がクリックされたら末尾要素を削除
-   $('#remove').on('click', function(){
-     num--;
-     $('li:last').remove();
- 
-     // 要素がなくなったら変数を0にする
-     if(num < 0){
-       num = 0;
-     }
-   });
+  // id属性がhideの要素がクリックされたら要素を非表示
+  $('#hide').on('click', function(){
+    $('ul').hide();
+  });
   });
